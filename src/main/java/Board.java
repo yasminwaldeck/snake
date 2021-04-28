f0f1c3708e02b740004a82f6509dde03d01f0198
yasminwaldeck <yasmin.waldeck@posteo.de>
2021-04-28 20:04:11 +0200

    Added several methods to board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,11 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Board {
+public class Board extends JPanel implements ActionListener {
 
     private Image ball;
     private Image food;
     private Image head;
+
+    private final int BACKGROUND_WIDTH = 450;
+    private final int BACKGROUND_HEIGHT = 450;
+
+    private int DOT_SIZE = 15;
+    private int TOTAL_DOTS = (450*450)/(15*15);
+    private int dots;
+
+    private Timer timer;
+    private int DELAY = 100;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = false;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+
+    private final int x[] = new int[TOTAL_DOTS];
+    private final int y[] = new int[TOTAL_DOTS];
+
 
     public Board(){
         initBoard();
@@ -13,7 +33,18 @@ public class Board {
 
     private void initBoard(){
 
-    }
+
+        setBackground(Color.darkGray);
+        setFocusable(true);
+        setPreferredSize(new Dimension(BACKGROUND_WIDTH, BACKGROUND_HEIGHT));
+        addKeyListener(new WSADAdapter());
+        loadImages();
+        initGame();
+
+        }
+
+
+
 
     private void loadImages() {
 
@@ -26,4 +57,68 @@ public class Board {
         ImageIcon iih = new ImageIcon("src/resources/head.png");
         head = iih.getImage();
     }
+
+    private void initGame(){
+        dots = 3;
+
+        for (int z = dots; z > 0; z--) {
+            x[z] = x[(z - 1)];
+            y[z] = y[(z - 1)];
+        }
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    private class WSADAdapter implements KeyListener {
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if ((key == KeyEvent.VK_A) && (!rightDirection)) {
+            leftDirection = true;
+            upDirection = false;
+            downDirection = false;
+        }
+
+        if ((key == KeyEvent.VK_D) && (!leftDirection)) {
+            rightDirection = true;
+            upDirection = false;
+            downDirection = false;
+        }
+
+        if ((key == KeyEvent.VK_W) && (!downDirection)) {
+            upDirection = true;
+            rightDirection = false;
+            leftDirection = false;
+        }
+
+        if ((key == KeyEvent.VK_S) && (!upDirection)) {
+            downDirection = true;
+            rightDirection = false;
+            leftDirection = false;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
+}
+
+
